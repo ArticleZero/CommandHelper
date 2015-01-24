c2abad29c9521bf1030dc2c3777105c979058d76
ArticleZero <articlezero@icloud.com>
2015-01-23 19:19:18 -0700

    Finish info

--- src/main/java/org/articlezero/commandhelper/Command.java
+++ src/main/java/org/articlezero/commandhelper/Command.java
@@ -1,8 +1,28 @@
 package org.articlezero.commandhelper;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author ArticleZero
  * @since 1/23/15
  */
-public class Command {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Command {
+
+    String name();
+    String permission();
+    //This is the message displayed when a user uses the command with incorrect usage
+    String help() default "&&cWrong usage, correct usage: &a%n%";
+    //This is if there is additional arguments, i.e /test command
+    String addRequiredArgs() default "";
+    //This is to add multiple permissions aside from the required one
+    String[] addPermissions() default {};
+    boolean allowConsole() default false;
+    //These are all the possible aliases _for the base command_
+    String[] aliases() default {};
+
 }
---
+++ src/main/java/org/articlezero/commandhelper/Handler.java
@@ -0,0 +1,16 @@
+package org.articlezero.commandhelper;
+
+import org.bukkit.command.*;
+import org.bukkit.command.Command;
+
+/**
+ * @author ArticleZero
+ * @since 1/23/15
+ */
+public class Handler implements CommandExecutor{
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String lable, String[] args) {
+        return false;
+    }
+}
